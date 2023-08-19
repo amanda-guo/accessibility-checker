104cff668a2b05165af51cb8f12bc543e4bd17bf
AdnxnHabib <adnanhabib263@gmail.com>
2023-08-19 12:44:14 -0400

    test commit

--- main.py
+++ main.py
@@ -35,4 +35,19 @@ Slider value: <|{value}|> <br/>
 
 """
 
+from taipy.gui import Gui
+import requests
+import json
+
+Gui(page = "BIG ASS DATA").run(dark_mode=True) # use_reloader=True
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+response = requests.get("https://wave.webaim.org/api/request?key=kw1MFx7w3380&reporttype=2&url=https://google.com/")
+jprint(response.json())
+
 Gui(page).run(use_reloader=True, port=5001)
