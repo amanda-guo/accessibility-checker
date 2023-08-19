3f41bca76cce77be28646ec55477b9f144b51f95
amanda-guo <aguo55@hotmail.com>
2023-08-19 12:19:52 -0400

    boilerplate for taipy app

---
+++ main.py
@@ -0,0 +1,38 @@
+from taipy.gui import Gui
+import pandas as pd
+
+value = 10
+
+page = """
+#Our Very First Taipy Application
+
+<|layout|columns=1 1 1|
+
+<|first column
+<|container container-styling|
+###Slider 1 <br/>
+Slider value: <|{value}|> <br/>
+<|{value}|slider|>
+|>
+|>
+
+<|second column
+<|container container-styling|
+###Slider 2 <br/>
+Slider value: <|{value}|> <br/>
+<|{value}|slider|>
+|>
+|>
+
+<|third column
+<|container container-styling|
+###Slider 3 <br/>
+Slider value: <|{value}|> <br/>
+<|{value}|slider|>
+|>
+|>
+|>
+
+"""
+
+Gui(page).run(use_reloader=True, port=5001)
